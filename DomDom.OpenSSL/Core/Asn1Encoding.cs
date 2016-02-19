7c8142fb37f6a66023e7c89c3be42a2fa80b44cf
C_B-A <C_B-A@f149d0f5-08f7-8743-b74d-e9091c64294d>
2016-02-19 02:09:34 +0000

    imported openssl implemntation small bugfixes
    git-svn-id: file:///H/Logs/temp/OpenSSL.Core_git@2 f149d0f5-08f7-8743-b74d-e9091c64294d

---
+++ DomDom.OpenSSL/Core/Asn1Encoding.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace DomDom.OpenSSL
+{
+	
+	[StructLayout(LayoutKind.Sequential)]
+	internal struct Asn1Encoding
+	{
+		public IntPtr enc;
+		public int len;
+		public int modified;
+	}
+}
+
